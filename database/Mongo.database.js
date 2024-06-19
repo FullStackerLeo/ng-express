a7a7d2d2946d8a7e331f2b5c6ac6067885e99807
aiXpertLab <aiXpertLab@gmail.com>
2024-06-19 08:43:13 -0400

    slice

--- database/Mongo.database.js
+++ database/Mongo.database.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose')
 const URI = process.env.MONGO_NG_URI
 mongoose.set('strictQuery', false)
 
-console.log('connecting to', URI.slice(0,23))
-
 async function connectToDatabase() {
   try {
     await mongoose.connect(URI);
